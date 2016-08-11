94d02db4163b926527e0720a450f9d286d7b0efd
shcn24 <n.v.shadrin@gmail.com>
2016-08-11 20:03:30 +0000

    change check run

--- router_restart/rr.sh
+++ router_restart/rr.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#folder=/etc/mysc/router_restart
-folder=/git/tests/router_restart/
+folder=/etc/mysc/router_restart
+#folder=/git/tests/router_restart/
 host1=8.8.8.80
 host2=ya.ruu
 host3=mail.ruu
 a=0
 b=0
 c=0
+#считываем количество попыток
 read count<$folder/count
-#r1up=/etc/mysc/cpio/relay/up1.sh
-#r1down=/etc/mysc/cpio/relay/down1.sh
-r1up="echo relay up"
-r1down="echo relay down"
+#пути к скриптам управления реле
+r1up=/etc/mysc/cpio/relay/up1.sh
+r1down=/etc/mysc/cpio/relay/down1.sh
+#r1up="echo relay up"
+#r1down="echo relay down"
 
-echo $count
-lc=$folder/lock
-
-#if [ -f $lc ]; then 
 #вычисляем количество запущенных копий нашей программы
 ch=`ps auxww |grep rr.sh |grep /bin/bash |wc |awk '{print $1-1}'`
 echo "процессов $ch"
@@ -25,10 +23,9 @@ echo "Process already running. Please wait and try again later."
 exit
     else
 {
-#date > $lc
 
 echo $count
-
+#проверяем пинги
 if ping -c 1 $host1; then let a=$a+1
     else a=0
     fi
@@ -50,8 +47,8 @@ if [ $d == 0 ]; then echo "Интернет не работает"
 	if [ $count -gt 5 ]; then
 		
 		$r1down
-		echo "уже было десять попыток. Делаю паузу"
-		sleep 1800s
+		echo "уже было 5 попыток. Делаю паузу 10 min"
+		sleep 60s
 		echo "0">$folder/count
 		
 	else
@@ -65,6 +62,5 @@ fi
 else echo "Интернет работает."
 echo "0">$folder/count
 fi
-#rm $lc
 }
 fi
